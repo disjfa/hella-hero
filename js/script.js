3c108b1bb48112479fbf7b4b45bc84ff7fc9d8dc
disjfa <disjfa@disjfa.nl>
2017-06-26 22:10:29 +0000

    Update 2017-06-26T22:10:27.861Z

--- js/script.js
+++ js/script.js
@@ -3,4 +3,12 @@ $(function () {
     $(evt.delegateTarget).siblings().removeClass('product_active');
     $(evt.delegateTarget).addClass('product_active');
   });
+
+  $('.box').on('click', 'input', function (evt) {
+    if(evt.currentTarget.checked) {
+      $(evt.delegateTarget).addClass('box_active');
+    } else {
+      $(evt.delegateTarget).removeClass('box_active');
+    }
+  });
 })
